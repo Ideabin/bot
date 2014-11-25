77dd91f666a5c194f151b48653672b87c3e2e771
Shadab Zafar <dufferzafar0@gmail.com>
2014-11-25 22:21:48 +0530

    Added database init and create_tables
    The database filename/path is read from the config (which is
    .gitignored)
    
    The table schema has not been finalized yet.

---
+++ db.py
@@ -0,0 +1,75 @@
+""" Provide database interactions to the bots. """
+
+import os
+import sqlite3
+from configparser import ConfigParser
+
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+config = ConfigParser()
+config.read(os.path.join(CURRENT_DIR, 'config.ini'))
+
+DATABASE = config["Database"]["File"]
+
+con = sqlite3.connect(DATABASE)
+
+
+def init():
+    """ Initialize the databse. """
+
+    with con:
+        create_tables(con)
+
+
+def create_tables(con):
+    """ Create tables. """
+
+    cur = con.cursor()
+    cur.execute(
+        """
+        CREATE TABLE IF NOT EXISTS users (
+            username    VARCHAR( 200 )      PRIMARY KEY
+                                            NOT NULL,
+
+            twitter     VARCHAR( 200 )      UNIQUE,
+
+            email       VARCHAR( 200 )      UNIQUE,
+
+            last_fetch  DATETIME
+        );
+        """
+    )
+
+    cur.execute(
+        """
+        CREATE TABLE IF NOT EXISTS ideas (
+            username     VARCHAR( 200 )     PRIMARY KEY
+                                            NOT NULL,
+
+            gistid       VARCHAR( 20 )      NOT NULL
+                                            UNIQUE,
+
+            description VARCHAR( 500 ),
+
+            FOREIGN KEY(username) REFERENCES users(username)
+        );
+        """
+    )
+
+    cur.execute(
+        """
+        CREATE TABLE IF NOT EXISTS tweets (
+            username     VARCHAR( 200 )     PRIMARY KEY
+                                            NOT NULL,
+
+            tweetid      VARCHAR( 20 )      NOT NULL
+                                            UNIQUE,
+
+            message      VARCHAR( 140 ),
+
+            FOREIGN KEY(username) REFERENCES users(username)
+        );
+        """
+    )
+
+    con.commit()
